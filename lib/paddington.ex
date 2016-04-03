167f3d7222f9472462af55cc955c114134980b4f
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-04-03 23:49:02 +0100

    update main supervisor to new structure

--- lib/paddington.ex
+++ lib/paddington.ex
@@ -1,5 +1,7 @@
 defmodule Paddington do
+  alias Paddington.Apps.Supervisor, as: AppsSupervisor
   alias Paddington.AppRegistry
+  alias Paddington.Configuration
   alias Paddington.Emitter
   alias Paddington.Listener
 
@@ -7,12 +9,28 @@ defmodule Paddington do
 
   def start(_type, _args) do
     if System.get_env("DEBUG"), do: :observer.start
+
+    configuration = "~/.paddington.yml"
+    |> Path.expand
+    |> Configuration.load!
+
+    start_application(configuration)
+  end
+
+  # Private implementation
+  ########################
+
+  defp start_application(configuration) do
     import Supervisor.Spec, warn: false
 
     children = [
-      worker(AppRegistry, []),
-      worker(Emitter, ["Launchpad Mini"]),
-      worker(Listener, ["Launchpad Mini"]),
+      # MIDI interfaces
+      worker(Emitter,             [configuration.device]),
+      worker(Listener,            [configuration.device]),
+
+      # apps
+      worker(AppRegistry,         []),
+      supervisor(AppsSupervisor, [configuration.applications]),
     ]
 
     opts = [strategy: :one_for_one, name: Paddington.Supervisor]
