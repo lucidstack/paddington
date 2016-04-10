a5c60868c15dbfed39b96fafc2cfcd89ec0acc55
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-04-10 13:26:31 +0100

    update yaml_elixir

--- mix.exs
+++ mix.exs
@@ -32,6 +32,6 @@ defmodule Paddington.Mixfile do
      {:mock, "~> 0.1.1", only: :test},
      {:credo, "~> 0.3.10", only: [:dev, :test]},
      {:yamerl, github: "yakaz/yamerl"},
-     {:yaml_elixir, "~> 1.0.0"}]
+     {:yaml_elixir, "~> 1.2.0"}]
   end
 end
