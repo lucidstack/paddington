3a3c3d2332a40c8ee1f7cc4d526ad1cefad3c458
Andrea Rossi <andrea.rossi@unep-wcmc.org>
2016-04-17 11:47:50 +0100

    bump version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Paddington.Mixfile do
 
   def project do
     [app: :paddington,
-     version: "0.0.1",
+     version: "1.0.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
